93f110230184dda655a02c55b34c20d6a140d127
Michelle <michelle.selva@gmail.com>
2024-10-31 22:30:53 -0300

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+import "./app.scss"
+
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Início"> 
+      <Navbar/>
+      
+      </section>
+    <section id="Sobre nós"> Sobre nós </section>
+    <section id="Serviços"> Serviços </section>
+    <section id="Contato"> Contato</section>
+     
+  </div>;
+ 
+
 };
 
 export default App;
